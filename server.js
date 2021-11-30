31457cc78dca5fd30d64815a729016f14bf29ba4
Kuba <kubawojewodzki46@gmail.com>
2021-11-30 22:03:53 +0100

    ready to deploy

--- server.js
+++ server.js
@@ -21,5 +21,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
+// set-up the port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
